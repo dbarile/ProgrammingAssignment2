ccfacb0d81981955abdd89671b33411ebb14c204
dbarile <dbarile@interserv.com>
2015-05-24 15:30:30 -0400

    Initial edit
    Initial edit after file download

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The two funtions illustrated below allow the inverse of a matrix to be calculated, cached and then recalled at a latter
+##time without having to recalculate the inverse again.
 
-makeCacheMatrix <- function(x = matrix()) {
+## First function
+## Take the passed matrix and create a list of functions to
+## Set the value of the matrix
+## Get the value of the matrix
+## Set the value of the inverse
+## Get the value of the inverse
 
-}
+makeCacheMatrix <- function(x = matrix()){
+        inv <- NULL                                       	#Initialize the 'inv' variable to NULL
+                
+        set <- function(y){                               	#Define function 'set'
+                x <<- y                                         #set the local 'x' to the passed arg 'y'    
+                inv <<- NULL                			#set 'inv' variable to NULL
+        }
+        
+        get <- function() x                                     #Define function 'get' for the makeCacheMatrix environ   
+
+        setinv <- function(inverse) inv <<- inverse             #Define function 'setinv' 
+                                                                #set variable 'inverse' equal to 'inv' from the 
+								#makeCacheMatrix environ 	
 
 
-## Write a short comment describing this function
+        getinv <- function() inv      				#Define function 'getinv' returns the value of 'inv'
+								#from the makeCacheMatrix environ                          
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+        list(set = set, get = get,                        	#Set the vaiables of the makeCacheMatrix environ 
+             setinv = setinv,					#into a list for calling
+             getinv = getinv)
 }
